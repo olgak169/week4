3acb0233715eeeb3ff2a096815fe50a69e1c54a9
olgak169 <okrupnova@gmail.com>
2020-11-30 15:35:00 +0600

    Week4

---
+++ SpeakGoodBye.js
@@ -0,0 +1,12 @@
+
+function speak(name) {
+  console.log(speakWord + " " + name);
+}
+(function (window) {
+ var speakWord = "Good Bye";
+  var byeSpeaker = {};
+   byeSpeaker.speak = function (name) {
+   console.log(speakWord + " " + name);
+  }
+  window.byeSpeaker = byeSpeaker;
+})(window);
---
+++ script.js
@@ -0,0 +1,19 @@
+
+(function () {
+ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+ console.log("Log");
+ for (let i = 0; i < names.length; i++) {
+
+  var firstLetter = names[i].charAt(0).toLowerCase();
+  
+ 
+  if (firstLetter == 'j') {
+  
+    byeSpeaker.speak(names[i])
+    
+  } else {
+ 
+    helloSpeaker.speak(names[i])
+  }
+}
+})();
