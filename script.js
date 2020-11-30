f0dadb67ca3b1c83a0ffc4740d0e391cae4bc5ae
olgak169 <okrupnova@gmail.com>
2020-11-30 15:39:11 +0600

    Week4

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 (function () {
  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
- console.log("Log");
+ 
  for (let i = 0; i < names.length; i++) {
 
   var firstLetter = names[i].charAt(0).toLowerCase();
